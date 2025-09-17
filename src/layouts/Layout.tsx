e244bab342861d6efe0b70acc0064f5f53188071
Kevin Soto <sotoorta93@gmail.com>
2025-09-16 22:29:02 -0300

    fix: correct import casing for N8nChat component in Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,8 @@
 import { Link, Outlet } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
-import N8nChat from "../components/N8Nchat";
+import N8nChat from "../components/N8nChat";
+
 
 
 
